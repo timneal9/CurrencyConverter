4d5f8bece138144a1cd7c66b96150fdcda22769c
Tim Neal <tim_neal@me.com>
2021-03-25 18:01:47 -0700

    Initial commit; Created Xcode project

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Tim Neal on 3/25/21.
+//  Copyright © 2021 Tim Neal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
