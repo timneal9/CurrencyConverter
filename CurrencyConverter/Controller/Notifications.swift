2c2f409b5c59015a952a6ab7784edde44979fc52
Tim Neal <tim_neal@me.com>
2021-07-21 21:01:57 -0700

    Adding flag images, resized imageviews

---
+++ CurrencyConverter/Controller/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  CurrencyConverter
+//
+//  Created by Tim Neal on 4/1/21.
+//  Copyright © 2021 Tim Neal. All rights reserved.
+//
+
+import Foundation
+
+class Notifications {
+    static let publishNotification = NSNotification.Name(rawValue: "VCPublishEvent")
+}
